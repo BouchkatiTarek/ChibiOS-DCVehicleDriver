2dba8f1eff2c3acce5437f06872bfd4e794d9923
Tarek <tarekbouchkati@gmail.com>
2013-07-20 04:37:52 -0700

    Update example.c

--- example.c
+++ example.c
@@ -13,30 +13,38 @@
 #include "ch.h"
 #include "hal.h"
 #include "dc_vehicle.h"
- 
-/*
- * First Method to declare a dc vehicle
- *
- * #include "dc_motor.h"
- * 
-static DCMotor RM = {
-}
- 
+#include "dc_motor.h"
 
-*/
- 
- static DCVehicle robot = { // robot configuration
-    { // Right motor
-    {GPIOA, 5},   // Right motor IN2 : PA5
-    {GPIOA, 4},   // Right motor IN1 : PA4
-    {GPIOA, 2},   // Right motor EN  : PA2
-    &PWMD2,       // Right motor EN PWM Driver : TIM2
-    2,            // Right motor EN PWM Channel : CH3
-    },
-    { // Left motor
-    {GPIOA, 6},   // Left motor IN1 : PA6
-    {GPIOA, 7},   // Left motor IN2 : PA7
-    {GPIOA, 3},   // Left motor EN  : PA3
-    &PWMD2,       // Left motor EN PWM Driver : TIM2
-    3,            // Left motor EN PWM Channel : CH4
-    }};
+static DCMotor rightMotor = {
+	{GPIOA, 4},   // Right motor IN1 : PA4
+	{GPIOA, 5},   // Right motor IN2 : PA5
+	{GPIOA, 2},   // Right motor EN  : PA2
+	&PWMD2,       // Right motor EN PWM Driver : TIM2
+	2             // Right motor EN PWM Channel : CH3
+}
+static DCMotor leftMotor = {
+	{GPIOA, 6},   // Left motor IN1 : PA6
+	{GPIOA, 7},   // Left motor IN2 : PA7
+	{GPIOA, 3},   // Left motor EN  : PA3
+	&PWMD2,       // Left motor EN PWM Driver : TIM2
+	3             // Left motor EN PWM Channel : CH4
+};
+
+static DCVehicle robot = { // robot configuration
+	rightMotor,
+	leftMotor
+};
+
+
+
+void main(void){
+	halInit();
+	chSysInit();
+	
+	DCVehicleConfig(&robot);
+	DCVehicleStop(&robot);
+	DCVehicleTurnRight(&robot, 80);
+	// ............
+	
+	while(TRUE);
+}
